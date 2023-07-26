c2b5d0643f0d1924ce4b4d126975afd79e2ecacd
jordanbrian155 <jordanbrian155@gmail.com>
2023-07-26 12:20:16 +0300

    first commit

--- javascript/signout.js
+++ javascript/signout.js
@@ -1,7 +1,7 @@
 document.getElementById("signout").onclick = function(){
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
-        window.location.href= "home.html"
+        window.location.href= "index.html"
       }).catch((error) => {
         // An error happened.
       });
